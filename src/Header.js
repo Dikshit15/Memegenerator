75babcbef10f2986e05da4bdb15a55bceb60b44f
Dikshit <dikshitmaheshwari15@gmail.com>
2019-11-10 23:35:04 +0530

    Minor changes

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
-
 // Since Header is a component which is only used to display, it should be a functional component
 
 function Header() {
-  return <h1>Header</h1>;
+  return (
+  <header>
+  <img
+    src='http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png'
+    alt='Problem?'
+    height='300'
+    width='300'
+    />
+    </header>
+  )
 }
 
 export default Header;
