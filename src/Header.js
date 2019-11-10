23e3ba79548ba3b991e973fb8dcd28855a9e373c
Dikshit <dikshitmaheshwari15@gmail.com>
2019-11-10 14:55:27 +0530

    Adding header file

---
+++ src/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+// Since Header is a component which is only used to display, it should be a functional component
+
+function Header() {
+  return <h1>HEADER</h1>;
+}
+
+export default Header;
